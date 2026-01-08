602b14d940ce00eb04802017e3f76bd75b97d1b7
Kate Isaac <41767733+kweav@users.noreply.github.com>
2026-01-08 16:47:17 -0500

    will you render

--- scripts/format-tables.R
+++ scripts/format-tables.R
@@ -5,6 +5,46 @@ library(tidyverse)
 library(DT)
 library(here)
 
+#' 
+#'
+
+add_modalities <- function(collection_df, modalitity_df){
+
+  joined_df <- full_join(collection_df, modality_df, by = c("name" = "Course"))
+  
+  joined_df %<>%
+    mutate(modality_icon = case_when(str_detect(modality_link, "youtu.be") ~ "<img src='https://www.iconpacks.net/icons/1/free-icon-cinema-832.png' width='15%'/>",
+                                     str_detect(modality_link, "buzzsprout") ~ "<img src='https://www.iconpacks.net/icons/4/free-icon-black-radio-microphone-14659.png' width='15%'/>",
+                                     str_detect(modality_link, "cheatsheets") ~ "<img src='https://www.iconpacks.net/icons/4/free-icon-to-do-list-13178.png' width='15%'/>"
+                                     ),
+           modality_type = case_when(str_detect(modality_link, "youtu.be") ~ "Video",
+                                     str_detect(modality_link, "buzzsprout") ~ "Podcast",
+                                     str_detect(modality_link, "cheatsheets") ~ "Cheatsheet"
+           )
+          ) %>%
+    mutate(modality_constructed_link = ifelse(is.na(modality_icon), NA, paste0('<a href="', modality_link, '" target="_blank"<div title="', modality_type, '"></div>', modality_icon, '</a><p class=\"image-name\">', modality_description, " ", modality_type, '</p>'))) %>%
+    arrange(modality_link) %>%
+    select(-c(modality_link, modality_icon, modality_type)) %>% #don't need anymore so dropping
+    pivot_wider(names_prefix = 'course_spec_mod_', 
+                names_from = modality_description, 
+                values_from = modality_constructed_link) %>%
+    select(-course_spec_mod_NA) %>%
+    unite(starts_with("course_spec_mod"), col = "MoreResources", na.rm = TRUE, remove = TRUE, sep = " <br/> ")
+  
+  #make the links with the icons
+  # look at the URL `Modality_Link` for "youtu.be", "buzzsprout", "cheatsheets" to pick the icon and put it first
+  #make a column that is the icon
+  
+  
+  #pivot wider so they're all lined up with the single course mentioned once (handling the unique problem)
+  
+  #make a single column/pasting them together in a single column for more resources and get rid of the original columns
+  
+  #build `More Resources` column based off of the number of rows with that course in joined_df
+  #ensure outputdf course names are unique
+return(joined_df)
+}
+
 #' Format the tibble/table to bold names, link links, add icons, and rearrange/select columns
 #'
 #' @description This function uses the CourseName, Funding, CurrentOrFuture, GithubLink, BookdownLink, CourseraLink, and LeanpubLink columns in order to make the first two columns
@@ -60,9 +100,9 @@ prep_table <- function(inputdf, current=TRUE, keep_category = FALSE){
     mutate(Concepts = str_replace_all(Concepts, "-", " ")) %>% #replace the hyphens/dashes with a space; special cases/substitutions are taken care of before rendering within index.Rmd
     mutate(BroadAudience = str_replace_all(BroadAudience, ";", "<br></br>")) %>%
     #Replace the broad audiences with logos
-    mutate(BroadAudience = str_replace(BroadAudience, "Software Developers", "<img src=\"resources/images/SoftwareDeveloper.png\" alt=\"Software Developers\" height=\"35\"></img><p class=\"image-name\">Software Developers</p>")) %>%
-    mutate(BroadAudience = str_replace(BroadAudience, "Researchers", "<img src=\"resources/images/NewToDataScience.png\" alt =\"Researchers\" height=\"35\"></img><p class=\"image-name\">Researchers</p>")) %>%
-    mutate(BroadAudience = str_replace(BroadAudience, "Leaders", "<img src=\"resources/images/leader_avataaars.png\" alt=\"Leaders\" height=\"35\"></img><p class=\"image-name\">Leaders</p>")) %>%
+    mutate(BroadAudience = str_replace(BroadAudience, "Software Developers", "<img src=\"resources/images/SoftwareDeveloper.png\" alt=\"Software Developers\" height=\"45\"></img><p class=\"image-name\">Software Developers</p>")) %>%
+    mutate(BroadAudience = str_replace(BroadAudience, "Researchers", "<img src=\"resources/images/NewToDataScience.png\" alt =\"Researchers\" height=\"45\"></img><p class=\"image-name\">Researchers</p>")) %>%
+    mutate(BroadAudience = str_replace(BroadAudience, "Leaders", "<img src=\"resources/images/leader_avataaars.png\" alt=\"Leaders\" height=\"45\"></img><p class=\"image-name\">Leaders</p>")) %>%
     #Replace the categories with logos
     mutate(Category = str_replace(Category, "Software Development", "<img src=\"resources/images/keyboard-1405.png\" alt=\"Software Development\" height=\"20\"></img><p class=\"image-name\">Software Development</p>")) %>%
     mutate(Category = str_replace(Category, "Best Practices", "<img src=\"resources/images/golden-cup-7825.png\" alt=\"Best Practices\" height=\"20\"></img><p class=\"image-name\">Best Practices</p>")) %>%
@@ -70,20 +110,17 @@ prep_table <- function(inputdf, current=TRUE, keep_category = FALSE){
     mutate(Category = str_replace(Category, "Hands-on Practice", "<img src=\"resources/images/practice.png\" alt=\"Hands-on Practice\" height=\"20\"></img><p class=\"image-name\">Hands-on Practice</p>"))
 
 
-  if ((keep_category) & (current)) { #select appropriate columns
-    outputdf %<>% select(c(CourseName, Funding, BroadAudience, description, Category, Concepts)) %>%
-      `colnames<-`(c("Course Name", "Funding", "Broad Audience", "Description", "Category", "Concepts Discussed"))
-  } else if ((keep_category) & !(current)){
+  if ((keep_category) & (current)) { #select appropriate columns keep the category column and only current courses
+    outputdf %<>% select(c(CourseName, Funding, BroadAudience, description, Category, Concepts, MoreResources)) %>%
+      `colnames<-`(c("Course Name", "Funding", "Broad Audience", "Description", "Category", "Concepts Discussed", "More Resources"))
+  } else if ((keep_category) & !(current)){ #keep the category column and only future courses
     outputdf %<>% select(c(CourseName, Funding, BroadAudience, description, Category)) %>%
       `colnames<-`(c("Course Name", "Funding", "Broad Audience", "Description", "Category"))
-  } else{
-    outputdf %<>% select(c(CourseName, Funding, BroadAudience, description, Concepts)) %>%
-      `colnames<-`(c("Course Name", "Funding", "Broad Audience", "Description", "Concepts Discussed"))
+  } else{ #drop the category column 
+    outputdf %<>% select(c(CourseName, Funding, BroadAudience, description, Concepts, MoreResources)) %>%
+      `colnames<-`(c("Course Name", "Funding", "Broad Audience", "Description", "Concepts Discussed", "More Resources"))
   }
-  outputdf$`More Resources` <- "Filler"
-  return(outputdf)
 }
-
 
 #' A function to setup the DT datatable
 #'
