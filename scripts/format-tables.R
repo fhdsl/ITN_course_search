dfcd24578fea255d5c6c1882797c50c2d8ec72b3
Kate Isaac <41767733+kweav@users.noreply.github.com>
2026-02-10 16:20:22 -0500

    try some more modalities

--- scripts/format-tables.R
+++ scripts/format-tables.R
@@ -5,41 +5,48 @@ library(tidyverse)
 library(DT)
 library(here)
 
-#' 
+#'
 #'
 
 add_modalities <- function(collection_df, modalitity_df){
 
   joined_df <- full_join(collection_df, modality_df, by = c("name" = "Course"))
-  
+
   joined_df %<>%
     mutate(modality_icon = case_when(str_detect(modality_link, "youtu.be") ~ "<img src='https://www.iconpacks.net/icons/1/free-icon-cinema-832.png' width='15%'/>",
                                      str_detect(modality_link, "buzzsprout") ~ "<img src='https://www.iconpacks.net/icons/4/free-icon-black-radio-microphone-14659.png' width='15%'/>",
-                                     str_detect(modality_link, "cheatsheets") ~ "<img src='https://www.iconpacks.net/icons/4/free-icon-to-do-list-13178.png' width='15%'/>"
+                                     str_detect(modality_link, "cheatsheets") ~ "<img src='https://www.iconpacks.net/icons/4/free-icon-to-do-list-13178.png' width='15%'/>",
+                                     str_detect(modality_link, "doi|articles") ~ "<img src='https://www.iconpacks.net/icons/1/free-icon-document-663.png' width='15%'/>",
+                                     str_detect(modality_link, "sciencecast") ~ "<img src='https://www.iconpacks.net/icons/4/free-icon-sound-on-14606.png' width='15%' />",
+                                     str_detect(modality_link, "hutchdatascience|docs.google") ~ "<img src='https://img.icons8.com/?size=100&id=24466&format=png&color=000000' width='15%' />"
                                      ),
            modality_type = case_when(str_detect(modality_link, "youtu.be") ~ "Video",
                                      str_detect(modality_link, "buzzsprout") ~ "Podcast",
-                                     str_detect(modality_link, "cheatsheets") ~ "Cheatsheet"
+                                     str_detect(modality_link, "cheatsheets") ~ "Cheatsheet",
+                                     str_detect(modality_link, "doi|articles") ~ "Publication",
+                                     str_detect(modality_link, "sciencecast") ~ "Soundbite",
+                                     str_detect(modality_link, "hutchdatascience|docs.google") ~ "Workshop material"
+
            )
           ) %>%
     mutate(modality_constructed_link = ifelse(is.na(modality_icon), NA, paste0('<a href="', modality_link, '" target="_blank"<div title="', modality_type, '"></div>', modality_icon, '</a><p class=\"image-name\">', modality_description, " ", modality_type, '</p>'))) %>%
     arrange(modality_link) %>%
     select(-c(modality_link, modality_icon, modality_type)) %>% #don't need anymore so dropping
-    pivot_wider(names_prefix = 'course_spec_mod_', 
-                names_from = modality_description, 
+    pivot_wider(names_prefix = 'course_spec_mod_',
+                names_from = modality_description,
                 values_from = modality_constructed_link) %>%
     select(-course_spec_mod_NA) %>%
     unite(starts_with("course_spec_mod"), col = "MoreResources", na.rm = TRUE, remove = TRUE, sep = " <br/> ")
-  
+
   #make the links with the icons
   # look at the URL `Modality_Link` for "youtu.be", "buzzsprout", "cheatsheets" to pick the icon and put it first
   #make a column that is the icon
-  
-  
+
+
   #pivot wider so they're all lined up with the single course mentioned once (handling the unique problem)
-  
+
   #make a single column/pasting them together in a single column for more resources and get rid of the original columns
-  
+
   #build `More Resources` column based off of the number of rows with that course in joined_df
   #ensure outputdf course names are unique
 return(joined_df)
@@ -116,7 +123,7 @@ prep_table <- function(inputdf, current=TRUE, keep_category = FALSE){
   } else if ((keep_category) & !(current)){ #keep the category column and only future courses
     outputdf %<>% select(c(CourseName, Funding, BroadAudience, description, Category)) %>%
       `colnames<-`(c("Course Name", "Funding", "Broad Audience", "Description", "Category"))
-  } else{ #drop the category column 
+  } else{ #drop the category column
     outputdf %<>% select(c(CourseName, Funding, BroadAudience, description, Concepts, MoreResources)) %>%
       `colnames<-`(c("Course Name", "Funding", "Broad Audience", "Description", "Concepts Discussed", "More Resources"))
   }
