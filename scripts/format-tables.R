be78e871b693788201bcc7b851cc7c1b65703fe1
Kate Isaac <41767733+kweav@users.noreply.github.com>
2026-02-11 14:41:08 -0500

    Update format-tables.R to be more robust
    using ends_with will be more robust than using contains

--- scripts/format-tables.R
+++ scripts/format-tables.R
@@ -52,7 +52,7 @@ add_modalities <- function(collection_df, modalitity_df){
     pivot_wider(names_prefix = 'course_spec_mod_',
                 names_from = modality_description,
                 values_from = modality_constructed_link) %>%
-    select(!contains("_NA")) %>%
+    select(!ends_with("_NA")) %>%
     unite(starts_with("course_spec_mod"), col = "MoreResources", na.rm = TRUE, remove = TRUE, sep = " <br/> ")
 
 return(joined_df)
